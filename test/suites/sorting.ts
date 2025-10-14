be84191e424f236862f36f1722873afbb81d0fa9
Sven James <sven@slink-tech.co.uk>
2025-10-14 16:52:03 +0100

    Increased testing timeout

--- test/suites/sorting.ts
+++ test/suites/sorting.ts
@@ -8,7 +8,7 @@ import { expect, ticketing } from '../setup'
 describe("Sorting", function(){
 
   //Set hook timeout
-  this.timeout(20000)
+  this.timeout(30000)
 
   before(async function(){
     //Create an event host
